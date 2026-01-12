d18fca4cc3fd120842f46bad6e00b8fe9d8dbe34
Abhishekke277 <abhishekkewat7566@gmail.com>
2026-01-12 20:22:17 +0530

    Secure MongoDB connection using environment variables

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
-const productRoutes = require("./routes/product.route.js");
 
 const app = express();
-const PORT = 3000;
 
-// Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false })); //the actual work of parsing the incoming data is done by body-parser
 
-// Routes
-app.use("/", productRoutes);
-
-
-
-
-// MongoDB connection
 mongoose
-  .connect("mongodb+srv://abhishekkewat7566_db_user:8Yf7CTvZJePsoSs7@firstclustor.0eoiecw.mongodb.net/firstclustor")
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("MongoDB Connected successfully");
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
+    console.log("MongoDB Connected");
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
   })
   .catch((err) => console.log(err));
